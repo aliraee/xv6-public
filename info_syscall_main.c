856ab108b612409e810e95777774b7e4ad132396
aliraee1378 <aliraee1@yahoo.com>
2020-06-08 23:16:32 +0430

    create main program for info syscall

---
+++ info_syscall_main.c
@@ -0,0 +1,24 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+struct proc_info {
+    int pid;
+    int memsize;
+ };
+int main(void)
+{
+	int rc =  fork();
+	if( rc == 0){
+		//child
+		int* childptr = (int *)malloc(1);
+		printf(1,"childpointer: %s\n", childptr );
+	}
+	else{
+		//parent
+		int* parentptr = (int *)malloc(1);
+		printf(1,"parentpointer : %s\n", parentptr);
+	}
+	info();
+	wait();
+	exit();
+}
