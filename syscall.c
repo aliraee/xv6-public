64b933c6456d46c978ec3cbf4b1fbb163db7ae7a
aliraee1378 <aliraee1@yahoo.com>
2020-07-06 18:20:48 +0430

    add waitx syscall to syscall.c

--- syscall.c
+++ syscall.c
@@ -104,7 +104,7 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_info(void);
-
+extern int sys_waitx(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
