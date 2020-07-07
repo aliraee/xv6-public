5d58de5cd14890945f1782a9b648abe276964657
aliraee1378 <aliraee1@yahoo.com>
2020-07-08 00:54:10 +0430

    implement waitx() using by wait()

--- proc.c
+++ proc.c
@@ -263,7 +263,7 @@ exit(void)
         wakeup1(initproc);
     }
   }
-
+  curproc->etime=ticks;
   // Jump into the scheduler, never to return.
   curproc->state = ZOMBIE;
   sched();
@@ -582,3 +582,48 @@ void info(void){
       
 
 }
+int 
+waitx(int *wtime ,int *rtime){
+  struct proc *p;
+  int havekids, pid;
+  struct proc *curproc = myproc();
+  
+  acquire(&ptable.lock);
+  for(;;){
+    // Scan through table looking for exited children.
+    havekids = 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->parent != curproc)
+        continue;
+      havekids = 1;
+      if(p->state == ZOMBIE){
+        // show proccess times
+        cprintf("pid=%d,StartTime=%d,EndTime=%d,RTime=%d,IOTime=%d,Ticks=%d\n",
+                p->pid,p->stime,p->etime,p->rtime,p->iotime,ticks);
+        *rtime=p->rtime;
+        *wtime=p->etime - (p->stime + p->iotime + p->rtime); //calculate waiting time
+        pid = p->pid; //helped from wait()
+        kfree(p->kstack);
+        p->kstack = 0;
+        freevm(p->pgdir);
+        p->pid = 0;
+        p->parent = 0;
+        p->name[0] = 0;
+        p->killed = 0;
+        p->state = UNUSED;
+        release(&ptable.lock);
+        return pid;
+      }
+    }
+
+    // No point waiting if we don't have any children.
+    if(!havekids || curproc->killed){
+      release(&ptable.lock);
+      return -1;
+    }
+
+    // Wait for children to exit.  (See wakeup1 call in proc_exit.)
+    sleep(curproc, &ptable.lock);  //DOC: wait-sleep
+  }
+}
+ 
